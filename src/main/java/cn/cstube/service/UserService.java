e05f6546ded52fc799b37d7e6f6be79f6a241376
HelingCode <929926540@qq.com>
2021-05-22 18:03:18 +0800

    init commit

---
+++ src/main/java/cn/cstube/service/UserService.java
@@ -0,0 +1,10 @@
+package cn.cstube.service;
+
+/**
+ * @auther heling
+ * @date 2021/5/22
+ */
+public interface UserService {
+    public void save();
+
+}
---
+++ src/main/java/cn/cstube/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package cn.cstube.service.impl;
+
+import cn.cstube.service.UserService;
+
+/**
+ * @auther heling
+ * @date 2021/5/22
+ */
+public class UserServiceImpl implements UserService {
+
+    public void save() {
+        System.out.println("user service running...");
+    }
+}
